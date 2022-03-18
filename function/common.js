cc038360b03e18d6f35d4c0981119b1ef2c15a2c
xwjune <xwjune@163.com>
2022-03-18 10:04:06 +0800

    update

--- function/common.js
+++ function/common.js
@@ -57,3 +57,56 @@ function convertFenToYuan(value) {
 
   return result;
 }
+
+// 判断数组是否相等
+const arrayEqual = (arr1 = [], arr2 = []) => {
+  return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]);
+};
+
+// 获取文件名前缀
+function getPrefix(filename) {
+  const pos = filename.lastIndexOf('.');
+  let prefix = filename;
+  if (pos !== -1) {
+    prefix = filename.substring(0, pos);
+  }
+  return prefix.trim();
+}
+
+// 获取文件名后缀
+function getSuffix(filename) {
+  const pos = filename.lastIndexOf('.');
+  let suffix = '';
+  if (pos !== -1) {
+    suffix = filename.substring(pos + 1);
+  }
+  return suffix.toLowerCase();
+}
+
+async function request(url, config) {
+  const options = {
+    method: config.method || 'POST',
+    mode: config.mode || 'cors',
+  };
+  if (config.method === 'POST') {
+    Object.assign(options, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (config.data) {
+      options.body = JSON.stringify(config.data);
+    }
+  }
+  if (config.headers) {
+    Object.assign(options, {
+      headers: {
+        ...options.headers,
+        ...config.headers,
+      },
+    });
+  }
+  const response = await fetch(url, options);
+  const result = await response.json();
+  return result;
+}
